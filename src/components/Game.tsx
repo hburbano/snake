79f0fbc4cb4f438484067d137cb43ee55b2654f3
Hans Burbano <hans.burbano@geniussportsmedia.com>
2020-02-10 19:13:15 -0500

    Add base components

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState, ReactElement } from 'react'
 
-const Game = () => {
-  const config = {}
+const Board = (row: number, cols: number): ReactElement => {
+  console.log({ row, cols })
+  return (
+    <p>
+      r{row} c{cols}
+    </p>
+  )
+}
 
-  return <div>{}</div>
+const Game = (): ReactElement => {
+  const state = useState({})
+  const config = {
+    rows: 50,
+    cols: 50,
+    tickDuration: 200,
+  }
+
+  const handleKeyPress = (event: KeyboardEvent): void => {
+    console.log(event)
+  }
+
+  const gameTick = (): void => {
+    console.log('tic-tac')
+  }
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', handleKeyPress)
+    window.gameTick = setInterval(() => {
+      gameTick()
+    }, config.tickDuration)
+
+    // Cleanup subscription on unmount
+    return (): void => {
+      document.body.removeEventListener('keydown', handleKeyPress)
+      clearInterval(window.gameTick)
+    }
+  }, [])
+
+  return <div>{Board(config.rows, config.cols)}</div>
 }
 
 export { Game }
